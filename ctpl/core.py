fcefd0c0276351a27608f0f2d10106e2c3f4c02f
candale <candale.andrei@gmail.com>
2021-11-10 15:49:25 +0200

    Updates to core

--- ctpl/core.py
+++ ctpl/core.py
@@ -3,13 +3,34 @@ from typing import Set, Callable, Dict
 import jinja2
 from docxtpl import DocxTemplate
 
+from . import filters
+
+
+__jinja_env = None  # noqa
+def get_jinja_env():  # noqa
+    global __jinja_env
+
+    if __jinja_env is None:
+        __jinja_env = jinja2.Environment(autoescape=False)
+        for name in dir(filters):
+            attr = getattr(filters, name)
+            if callable(attr) and getattr(attr, '_is_filter', None):
+                __jinja_env.filters[name] = attr
+
+    return __jinja_env
+
+
+def register_filter(name: str, func):
+    env = get_jinja_env()
+    env.filters[name] = func
+
 
 def get_argumnets_from_docx(doc: DocxTemplate) -> Set[str]:
     xml = doc.get_xml()
     xml = doc.patch_xml(xml)
     xml = xml.replace(r'<w:p>', '\n<w:p>')
 
-    env = jinja2.Environment(autoescape=False)
+    env = get_jinja_env()
     ast = env.parse(xml)
 
     return jinja2.meta.find_undeclared_variables(ast)
@@ -37,5 +58,5 @@ def build_docx_context_from_cli(
 
 
 def save_rendered(doc: DocxTemplate, context: dict, path: str):
-    doc.render(context)
+    doc.render(context, jinja_env=get_jinja_env())
     doc.save(path)
